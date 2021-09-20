0d1b3be486e1a5afbe5806cfb00c0efb744db0c9
Mihail Nazarov <msnazarow@gmail.com>
2021-09-20 14:27:32 +0300

    feat(ex01)

---
+++ ex01/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by out-nazarov2-ms on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Click me", for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(button)
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.bottomAnchor.constraint(equalTo: button.topAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
+    @objc private func didButtonTapped() {
+        label.text = "Hello World!"
+        print("Hello World!")
+    }
+}
+
